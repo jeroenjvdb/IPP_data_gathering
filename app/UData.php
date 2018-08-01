4f751fe42fe87a61b07f48e513c149d535d9cf34
jeroen van den broeck <jeroen_vdb1@hotmail.com>
2018-08-01 16:54:07 +0200

    error in attributes

--- app/UData.php
+++ app/UData.php
@@ -27,10 +27,10 @@ class UData extends Model
     ];
 
     public function getCloggedAttribute() {
-        $this->attributes['status'] == 1;
+        return $this->status == 1;
     }
 
     public function getCongestionAttribute() {
-        $this->attributes['status'] == 2;
+        return $this->status == 2;
     }
 }
